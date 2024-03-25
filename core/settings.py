e9627529dc8f1666b400afd1c1932235546ab129
bahrom04 <magdiyevbahrom@gmail.com>
2024-03-25 11:59:16 +0500

    reset ass

--- core/settings.py
+++ core/settings.py
@@ -9,10 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
