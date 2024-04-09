1e85c6c0f3a3ff8b1cb1cc5934a7e010f09036e6
bahrom04 <magdiyevbahrom@gmail.com>
2024-04-09 11:42:54 +0500

    update ci-cd

--- core/settings/base.py
+++ core/settings/base.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["bahrombek.uz"]
 
 
 # Application definition
