1b8eb4fcca04b8a6c70f5162ce4f1a195a76df7d
bahrom04 <magdiyevbahrom@gmail.com>
2024-04-10 16:14:06 +0500

    allowed hosts

--- core/settings/base.py
+++ core/settings/base.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["bahrombek.uz"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
