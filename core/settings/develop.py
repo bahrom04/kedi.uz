5a47de65423a451c45f840b9198436bc09f02c05
bahrom04 <magdiyevbahrom@gmail.com>
2024-05-09 07:54:39 +0500

    feat: testing without docker in dev mode

--- core/settings/develop.py
+++ core/settings/develop.py
@@ -2,11 +2,15 @@ from core.settings.base import *
 
 DEBUG = True
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
 
 ALLOWED_HOSTS = ["*"]
