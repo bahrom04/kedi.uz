c6953bdf5060f6604519554869a224bf3bdf99fa
bahrom04 <magdiyevbahrom@gmail.com>
2024-05-09 07:44:26 +0500

    fix database

--- core/settings/base.py
+++ core/settings/base.py
@@ -87,7 +87,7 @@ DATABASES = {
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        # "PORT": os.getenv("DB_PORT"),
     }
 }
 
