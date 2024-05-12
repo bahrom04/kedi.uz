8432620bc595dbd1f925f063a39ec5d8af25fd22
bahrom04 <magdiyevbahrom@gmail.com>
2024-05-12 14:56:43 +0500

    feat: uzbek language set to the default if there is no django_language in cookie

--- core/settings/base.py
+++ core/settings/base.py
@@ -49,7 +49,8 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
+    # "django.middleware.locale.LocaleMiddleware",
+    "core.middleware.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "uz"
 LANGUAGES = (
     ("en", "English"),
     ("uz", "Uzbek"),
