c0c0b3094b95a94e136f7893d99fae5a10bc9a09
bahrom04 <magdiyevbahrom@gmail.com>
2024-06-13 14:09:17 +0500

    feat: new redesign layout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+python manage.py tailwind install --no-input
+python manage.py tailwind build --no-input
 
 # Compile new translations
 echo "Compile Messages"
@@ -8,7 +10,6 @@ python manage.py compilemessages --noinput
 # Collect static files
 echo "Collect static files"
 python manage.py collectstatic --noinput
-
 
 # Apply database migrations
 echo "Apply database migrations"
