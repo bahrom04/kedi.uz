036f51d1e3e1b722aca9599104f42f4c1a887a3c
bahrom04 <magdiyevbahrom@gmail.com>
2024-06-18 11:17:00 +0500

    feat: set_language by input and select intead of post

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.i18n import set_language
+from django.conf.urls.i18n import i18n_patterns
 from django.views.generic.base import TemplateView  # new
 
 from apps.common.api_endpoints.views import api as common
@@ -13,7 +15,7 @@ from apps.common.api_endpoints.views import api as common
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("ckeditor/", include("ckeditor_uploader.urls")),
-    path("i18n/", include("django.conf.urls.i18n")),
+    path("set_language/", set_language, name="set_language"),
     path(
         "ckeditor/upload/",
         serve,
@@ -27,6 +29,8 @@ urlpatterns = [
     ),
     path("api/", common.urls),
 ]
+
+urlpatterns += i18n_patterns(path("", include("apps.common.urls")))
 
 if settings.DEBUG:
     urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
