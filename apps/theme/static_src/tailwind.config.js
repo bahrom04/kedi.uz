3920677447ac797f51e278010ab2c5b26dfd6c9f
bahrom04 <magdiyevbahrom@gmail.com>
2024-06-20 22:59:45 +0500

    responsive tailwind properties

--- apps/theme/static_src/tailwind.config.js
+++ apps/theme/static_src/tailwind.config.js
@@ -43,6 +43,13 @@ module.exports = {
     ],
     theme: {
         extend: {},
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+          },
     },
     plugins: [
         /**
