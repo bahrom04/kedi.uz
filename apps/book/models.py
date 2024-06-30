d53f92547a4826150186097ee0a1a74f7c0aae81
bahrom04 <magdiyevbahrom@gmail.com>
2024-06-30 09:11:52 +0500

    error: migrations 5 change field

--- apps/book/models.py
+++ apps/book/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from apps.common import models as common
 
-from ckeditor_uploader.fields import RichTextUploadingField
-
-
 class Community(common.BaseModel):
     title = models.CharField(_("Title"), max_length=128)
     image = models.ImageField(upload_to="community/")
