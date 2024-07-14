17d303406a0a35b4a28b2688a40900c713386a90
Shavkatjon-O <shavkatjonodamboyev@gmail.com>
2024-07-15 00:58:32 +0500

    fix

--- core/settings/base.py
+++ core/settings/base.py
@@ -207,11 +207,6 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 
 # sentry_sdk.init(
 #     dsn=f"https://{sentry_key}.ingest.us.sentry.io/4507560080900096",
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
 #     traces_sample_rate=1.0,
-#     # Set profiles_sample_rate to 1.0 to profile 100%
-#     # of sampled transactions.
-#     # We recommend adjusting this value in production.
 #     profiles_sample_rate=1.0,
 # )
