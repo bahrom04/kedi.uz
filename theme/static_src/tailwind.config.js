fd51c69021a75f9c10fe8c93339fdea6892c9e02
bahrom04 <magdiyevbahrom@gmail.com>
2024-11-03 00:03:14 +0500

    update lost pet list ui

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -52,6 +52,7 @@ module.exports = {
          */
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
     ],
 }
