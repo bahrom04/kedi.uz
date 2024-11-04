a3ea46185de4bcd82b6f0ec81670178e069808dd
bahrom04 <magdiyevbahrom@gmail.com>
2024-11-04 22:01:47 +0500

    feat entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# Activate the virtual environment
+source venv/bin/activate
+
 python manage.py migrate
 python manage.py tailwind install --no-input
 python manage.py tailwind build --no-input
 python manage.py collectstatic --no-input
 python manage.py compilemessages
 
+# Restart services
+sudo systemctl restart gunicorn
+sudo systemctl restart nginx
