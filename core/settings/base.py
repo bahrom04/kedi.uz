dc9ad425c15bbb4c0ae7139de0bf7184838f48d4
bahrom04 <magdiyevbahrom@gmail.com>
2025-02-05 20:29:55 +0500

    fix: DisallowedHost error

--- core/settings/base.py
+++ core/settings/base.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["kedi.uz", "localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
