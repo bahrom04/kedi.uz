910d576a39b79a4355117bc0b6f098a2fc21e3d7
bahrom04 <magdiyevbahrom@gmail.com>
2025-05-13 00:12:10 +0500

    remove: postgres settings because already exist in base.py

--- core/settings/develop.py
+++ core/settings/develop.py
@@ -2,15 +2,4 @@ from core.settings.base import *
 
 DEBUG = True
 
-DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
-}
-
 ALLOWED_HOSTS = ["*"]
